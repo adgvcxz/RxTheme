6e420cf0e0b0acbd18c1c62122d742372e905c6c
adgvcxz <344292600@qq.com>
2017-07-10 17:29:56 +0800

    import kotlin

---
+++ app/src/main/java/com/adgvcxz/rxtheme/sample/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.adgvcxz.rxtheme.sample
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * zhaowei
+ * Created by zhaowei on 2017/7/10.
+ */
+class MainActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
